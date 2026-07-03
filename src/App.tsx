904ce6b10e35e2a94fac84f9d1ce1f20468d48ae
SangTran <112836164+sangtranresola@users.noreply.github.com>
2026-07-03 11:50:30 +0700

    update script

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import './App.css'
 
+// https://sangtranresola.github.io/cw-client-test-prod/
+
 declare global {
   interface Window {
     __DECA_CLIENT__01j19dwrdd4nhwey9661cvwbh1: any;
+    __DECA_CLIENT__01kwjtk51hyf11teqa4szf6z9r: any;
   }
 }
 
 function App() {
-  const openCWwbh1= () => {
-    window.__DECA_CLIENT__01j19dwrdd4nhwey9661cvwbh1.chat.toggleChatWindow();
+  // const openCWwbh1= () => {
+    // window.__DECA_CLIENT__01j19dwrdd4nhwey9661cvwbh1.chat.toggleChatWindow();
+  // };
+
+  const sendCustomerData= () => {
+    window.__DECA_CLIENT__01kwjtk51hyf11teqa4szf6z9r.chat.setVariable("userInfo", {
+      "customerName": "John Doe",
+      "customerId": "zxy39x4a3olq5",
+    });
   };
+
+  const sendNavigationsLog= () => {
+    const pages = ['url1.com', 'url2.com', 'url3.com'];
+    window.__DECA_CLIENT__01kwjtk51hyf11teqa4szf6z9r.chat.setVariable("pages", pages.join(', '));
+  };
+
+  
 
   return (
     <>
       <div className='flex col gap-4 mb-4'>
-        <h1>CW Client Production DEV TEST 1111</h1>
+        <h1>CW Client Production</h1>
       </div>
       <div className='flex col gap-4 mb-4'>
-        <button
+        {/* <button
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
           onClick={openCWwbh1}>
           OPEN CW ID: ...wbh1 
+        </button> */}
+        <button
+          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+          onClick={sendCustomerData}>
+          Send CustomerInfo
+        </button>
+        <button
+          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+          onClick={sendNavigationsLog}>
+          Send Navigations Log
         </button>
       </div>
     </>
